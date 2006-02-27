de34bbbf1a716be5c5f7340b830b13b092d8258e
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-02-27 23:00:03 +0000

    - Changing apps to standalone application

--- apps/dot_mcp/menu.c
+++ apps/dot_mcp/menu.c
@@ -3,23 +3,24 @@
  * 
  * Copyright (C) 2004-2006 Simon Wunderlich <dotslash@packetmixer.de>
  *
- * This file is part of s3d, a 3d network display server.
+ * This file is part of dot_mcp, a mcp for s3d.
  * See http://s3d.berlios.de/ for more updates.
  * 
- * s3d is free software; you can redistribute it and/or modify
+ * dot_mcp is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * 
- * s3d is distributed in the hope that it will be useful,
+ * dot_mcp is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with s3d; if not, write to the Free Software
+ * along with dot_mcp; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 
 
 #include "s3d.h"
--- apps/s3d_x11gate/s3d_x11gate.c
+++ apps/s3d_x11gate/s3d_x11gate.c
@@ -3,21 +3,21 @@
  * 
  * Copyright (C) 2004-2006 Simon Wunderlich <dotslash@packetmixer.de>
  *
- * This file is part of s3d, a 3d network display server.
+ * This file is part of s3d_x11gate, a 3d gateway for x11 desktops.
  * See http://s3d.berlios.de/ for more updates.
  * 
- * s3d is free software; you can redistribute it and/or modify
+ * s3d_x11gate is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * 
- * s3d is distributed in the hope that it will be useful,
+ * s3d_x11gate is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with s3d; if not, write to the Free Software
+ * along with s3d_x11gate; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
