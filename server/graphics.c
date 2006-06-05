8edaaeab8887aa7d4c88ff2ca35974e53d52aecc
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-06-05 15:19:15 +0000

    - more TODO - move light position again

--- server/graphics.c
+++ server/graphics.c
@@ -350,7 +350,7 @@ void graphics_main()
 	struct t_process *p=get_proc_by_pid(MCP);
 	t_mtrx m;
 /*	GLfloat pos[]={100.0,20.0,100.0,1.0};*/
-	GLfloat pos[]={0,20,1.0,1.0};
+	GLfloat pos[]={0,50,50,1.0};
 	GLfloat light0_spec[]={0.7,0.7,0.7,0.0};
 	GLfloat light0_shininess[] ={50.0};
 	GLfloat light0_diff[]={0.5,0.5,0.5,1.0};
