726fcae117a9093bfe1290fb0f1d3fd73a331ff9
Andreas Langer <nichtsnutz@berlios.de>
2006-09-18 18:19:09 +0000

    change license

--- apps/olsrs3d/search.h
+++ apps/olsrs3d/search.h
@@ -5,19 +5,19 @@
  *
  * This file is part of the olsrs3d, an olsr topology visualizer for s3d.
  * See http://s3d.berlios.de/ for more updates.
- * 
- * s3d Widgets is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
+ *
+ * s3d is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
- * s3d Widgets is distributed in the hope that it will be useful,
+ *
+ * s3d is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with the s3d Widgets; if not, write to the Free Software
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s3d; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
