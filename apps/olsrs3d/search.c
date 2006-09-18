bd538e4aeeb4535680bc7f2b9b928d1db7ab7022
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-09-18 18:24:30 +0000

    - some more political correctness in olsrs3d copyleft headers

--- apps/olsrs3d/search.c
+++ apps/olsrs3d/search.c
@@ -6,18 +6,18 @@
  * This file is part of the olsrs3d, an olsr topology visualizer for s3d.
  * See http://s3d.berlios.de/ for more updates.
  *
- * s3d is free software; you can redistribute it and/or modify
+ * olsrs3d is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * s3d is distributed in the hope that it will be useful,
+ * olsrs3d is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with s3d; if not, write to the Free Software
+ * along with olsrs3d; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
