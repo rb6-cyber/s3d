77d9763196b2ae423fec89c602021bcd73cf9bc4
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-10-19 18:27:59 +0000

    - because it's so funny: here is the perl binding (of course incomplete, no callbacks etc)

---
+++ swig/s3d_test.pl
@@ -0,0 +1,12 @@
+use s3d;
+if (0==s3d::s3dpy_init("hello perl"))
+{
+	s3d::s3d_select_font("vera");
+	$o= s3d::s3dpy_draw_string("hello from perl");
+	print("object $o\n");
+	s3d::s3d_flags_on($o,$s3d::S3D_OF_VISIBLE);
+	sleep(10);
+	s3d::s3d_quit();
+	
+	
+}
