bdac228c8817396bd202398fabd54390ce728786
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-11-28 12:46:45 +0000

    - text above the spheres (somehow ;)

--- apps/kism3d/gui.c
+++ apps/kism3d/gui.c
@@ -139,7 +139,7 @@ int handle_networks() {
 
 
 			wlan_network->scale_fac = wlan_network->num_wlan_clients + 2;
-			s3d_translate( wlan_network->wrsphr_id, 0, -6 + wlan_network->scale_fac, wlan_network->scale_fac );
+			s3d_translate( wlan_network->wrsphr_id, 0, /*-6 + wlan_network->scale_fac*/ 0, 0);
 			s3d_scale( wlan_network->obj_id, wlan_network->scale_fac );
 
 			real_node_pos_x = sin( 2.0* M_PI * network_index / ((float) Num_networks ) ) * ( ( ( 1 * Num_networks ) / 2 * M_PI ) );
@@ -172,7 +172,7 @@ int handle_networks() {
 
 					wlan_network->bssid_id = s3d_draw_string( wlan_network->bssid, &wlan_network->bssid_len );
 					s3d_link( wlan_network->bssid_id, wlan_network->obj_id );
-					s3d_translate( wlan_network->bssid_id, - wlan_network->bssid_len / 2, -2, 0 );
+					s3d_translate( wlan_network->bssid_id, - wlan_network->bssid_len / 2, 2+ wlan_network->scale_fac, 0 );
 					s3d_scale( wlan_network->bssid_id, BSSID_SCALE );
 					s3d_flags_on( wlan_network->bssid_id, S3D_OF_VISIBLE );
 
@@ -180,7 +180,7 @@ int handle_networks() {
 
 				wlan_network->ssid_id = s3d_draw_string( wlan_network->ssid, NULL );
 				s3d_link( wlan_network->ssid_id, wlan_network->bssid_id );
-				s3d_translate( wlan_network->ssid_id, 0, -2, 0 );
+				s3d_translate( wlan_network->ssid_id, 0, -1, 0 );
 				s3d_flags_on( wlan_network->ssid_id, S3D_OF_VISIBLE );
 
 				label_str = alloc_memory( 100 );
@@ -188,7 +188,7 @@ int handle_networks() {
 
 				wlan_network->misc_id = s3d_draw_string( label_str, NULL );
 				s3d_link( wlan_network->misc_id, wlan_network->ssid_id );
-				s3d_translate( wlan_network->misc_id, 0, -2, 0 );
+				s3d_translate( wlan_network->misc_id, 0, -1, 0 );
 				s3d_flags_on( wlan_network->misc_id, S3D_OF_VISIBLE );
 
 			}
@@ -211,7 +211,7 @@ int handle_networks() {
 
 			s3d_rotate( wlan_network->bssid_id, 0, angle , 0 );
 
-			s3d_translate( wlan_network->bssid_id, -cos(angle_rad) * BSSID_SCALE * wlan_network->bssid_len / 2 ,-1.5, sin(angle_rad) * BSSID_SCALE * wlan_network->bssid_len / 2 );
+			s3d_translate( wlan_network->bssid_id, -cos(angle_rad) * BSSID_SCALE * wlan_network->bssid_len / 2 ,2 , sin(angle_rad) * BSSID_SCALE * wlan_network->bssid_len / 2 );
 
 			wlan_network->rotation = ( wlan_network->rotation + 1 ) % 360;
 			s3d_rotate( wlan_network->wrsphr_id, 0, wlan_network->rotation, 0 );
