d9cd22f1f0e24556bd715d410115e8654af5bb06
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2006-12-26 19:56:05 +0000

    - forgot to add allocate.h - sorry

---
+++ server/allocate.h
@@ -0,0 +1,33 @@
+/*
+ * allocate.h
+ *
+ * Copyright (C) 2006 	Marek Lindner <lindner_marek@yahoo.de>
+ * 						Simon Wunderlich <dotslash@packetmixer.de>
+ * 						(probably more people from the batman project)
+ *
+ * This file is part of s3d, a 3d network display server.
+ * See http://s3d.berlios.de/ for more updates.
+ * 
+ * s3d is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * s3d is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with s3d; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* this file is taken from the batman project (www.open-mesh.net/batman)
+ * to find heap corruptions... */
+
+void checkIntegrity(void);
+void checkLeak(void);
+void *debugMalloc(unsigned int length, int tag);
+void *debugRealloc(void *memory, unsigned int length, int tag);
+void debugFree(void *memoryParameter);
