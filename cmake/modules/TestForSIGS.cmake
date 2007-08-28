ef187f8f6e082471393bfed502d8a2410aa10470
Sven Eckelmann <sven.eckelmann@gmx.de>
2007-08-28 15:50:10 +0000

    Test for sig_t to enable compilation under cygwin

--- cmake/modules/TestForSIGS.cmake
+++ cmake/modules/TestForSIGS.cmake
@@ -1,2 +1,13 @@
 include(CheckFunctionExists)
-check_function_exists(signal HAVE_SIGS)
+check_function_exists(signal HAVE_SIGS_FUNC)
+
+include(CheckCSourceCompiles)
+check_c_source_compiles("#include<signal.h>
+	int main() {
+	sig_t test;
+	return 0;
+}" SIG_T_DEFINED)
+
+if (HAVE_SIGS_FUNC AND SIG_T_DEFINED)
+	set(HAVE_SIGS TRUE)
+endif (HAVE_SIGS_FUNC AND SIG_T_DEFINED)
