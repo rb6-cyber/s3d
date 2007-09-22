9618c5f680898461baf4fd7898225fc5d04a515d
Sven Eckelmann <sven.eckelmann@gmx.de>
2007-09-22 16:06:25 +0000

    Changed license of http_* from Lyle Hanson to GPL2
    Files are used in a GPL2 program and are now licensed under GPL2 according to LGPL2 section 3

--- apps/s3dosm/http_error_codes.h
+++ apps/s3dosm/http_error_codes.h
@@ -1,19 +1,23 @@
 /* http_error_codes.h - Error code definitions
-
- Copyright (C) 2001 Lyle Hanson (lhanson@cs.nmu.edu)
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public
- License as published by the Free Software Foundation; either
- version 2 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
-
- See LICENSE file for details
-
+ *
+ * Copyright (C) 2001 Lyle Hanson (lhanson@cs.nmu.edu)
+ *
+ * This file is part of s3dosm, a gps card application for s3d.
+ * See http://s3d.berlios.de/ for more updates.
+ *
+ * s3dosm is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s3dosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s3dosm; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef HTTP_ERROR_CODES_H
--- apps/s3dosm/http_fetcher.c
+++ apps/s3dosm/http_fetcher.c
@@ -1,23 +1,28 @@
 /* http_fetcher.c - HTTP handling functions
-
- Copyright (C) 2001 Lyle Hanson (lhanson@cs.nmu.edu)
-
- This library is free software; you can redistribute it and/or
- modify it under the terms of the GNU Library General Public
- License as published by the Free Software Foundation; either
- version 2 of the License, or (at your option) any later version.
-
- This library is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- Library General Public License for more details.
-
- See included LICENSE file for details
-
- Changes:
-  Simon Wunderlich <dotslash@packetmixer.de>
-  + added http_setAuth() to support basic http-authentication and some minor fixes
-
+ *
+ * Copyright (C) 2001 Lyle Hanson (lhanson@cs.nmu.edu)
+ *
+ * This file is part of s3dosm, a gps card application for s3d.
+ * See http://s3d.berlios.de/ for more updates.
+ *
+ * s3dosm is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s3dosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s3dosm; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *
+ * Changes:
+ *  Simon Wunderlich <dotslash@packetmixer.de>
+ *  + added http_setAuth() to support basic http-authentication and some minor fixes
  */
 
 #include <stdlib.h>
