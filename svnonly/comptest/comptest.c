203f2fca820aa8b32c465ad1f3912507ad47a8be
Simon Wunderlich <siwu@hrz.tu-chemnitz.de>
2007-11-02 23:28:18 +0000

    - change copyright statement (comptest is not olsrs3d! O_o)

--- svnonly/comptest/comptest.c
+++ svnonly/comptest/comptest.c
@@ -6,18 +6,18 @@
  * This file is part of comptest, a proof-of-concept composite manager hack.
  * See http://s3d.berlios.de/ for more updates.
  *
- * olsrs3d is free software; you can redistribute it and/or modify
+ * comptest is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * olsrs3d is distributed in the hope that it will be useful,
+ * comptest is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with olsrs3d; if not, write to the Free Software
+ * along with comptest; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
