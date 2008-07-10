9fb75e56d30b8ca384ed8694273b68386ef161a2
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-07-10 16:55:16 +0000

    Use list(APPEND ..,) instead of set to append item to variable in cmake

--- cmake/modules/SetC9X.cmake
+++ cmake/modules/SetC9X.cmake
@@ -3,7 +3,7 @@ include(CheckCCompilerFlag)
 # Enable c99/c9x
 check_c_compiler_flag("-std=c9x" SUPPORT_C9X)
 if (SUPPORT_C9X)
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=c9x ")
+	list(APPEND CMAKE_C_FLAGS "-std=c9x ")
 else (SUPPORT_C9X)
 	message(STATUS "Could not enable c9x support")
 endif (SUPPORT_C9X)
