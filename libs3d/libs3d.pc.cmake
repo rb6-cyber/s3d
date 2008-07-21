53641592fc7850ad71096e2644ba2f910cacc427
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-07-21 19:51:07 +0000

    Add sdl to requirements of libs3d in pkg-config

--- libs3d/libs3d.pc.cmake
+++ libs3d/libs3d.pc.cmake
@@ -6,6 +6,6 @@ includedir=@CMAKE_INSTALL_PREFIX@/include
 Name: libs3d
 Version: @VERSION@
 Description: Client library for the s3d server
-Requires: libg3d freetype2 fontconfig
+Requires: libg3d freetype2 fontconfig sdl
 Libs: -L${libdir} -ls3d
 Cflags: -I${includedir}
