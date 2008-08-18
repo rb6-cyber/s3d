17af73ee885036f3771f612028e14ff528d64b45
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-08-18 10:08:46 +0000

    Add missing license headers

--- apps/s3dosm/nav.c
+++ apps/s3dosm/nav.c
@@ -1,3 +1,26 @@
+/*
+ * nav.c
+ *
+ * Copyright (C) 2006 Simon Wunderlich <dotslash@packetmixer.de>
+ *
+ * This file is part of s3dosm, a gps card application for s3d.
+ * See http://s3d.berlios.de/ for more updates.
+ *
+ * s3dosm is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * s3dosm is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with s3dosm; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "s3dosm.h"
 #include <s3d.h>
 #include <string.h> /* strcmp() */
--- extras/swig/s3d_test.py
+++ extras/swig/s3d_test.py
@@ -1,4 +1,24 @@
 #!/usr/bin/python
+# -*- coding: utf-8; -*-
+
+# Copyright (C) 2007 Simon Wunderlich <dotslash@packetmixer.de>
+#
+# See http://s3d.berlios.de/ for more updates.
+#
+# testvis is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# testvis is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with testvis; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 from s3d import *
 from time import sleep
 if 0 == s3dpy_init("hello world"):
