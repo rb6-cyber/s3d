bdf374fff4fabc6c256c6d85663a42e82e3a59de
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-08-25 09:50:51 +0000

    Use rfc822 email addresses in copyright headers

--- libs3d/hash.h
+++ libs3d/hash.h
@@ -2,7 +2,7 @@
  * hash.h
  *
  * Copyright (C) 2004-2007 Simon Wunderlich <dotslash@packetmixer.de>
- *          Marek Lindner
+ *                         Marek Lindner <lindner_marek@yahoo.de>
  *
  * This file is part of the s3d API, the API of s3d (the 3d network display server).
  * See http://s3d.berlios.de/ for more updates.
