d2375e0e1634b4620eba305e61eea5edced8a24c
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-09-01 19:08:08 +0000

    Remove date and time from manpage comment
    This should reduce the changed files when running build-manpages without
    significant changed data sources.

--- Documentation/manpages/build-manpages.sh
+++ Documentation/manpages/build-manpages.sh
@@ -4,6 +4,7 @@ for i in 1 3 9; do
 	for man in man${i}/*.sgml; do
 		if [ -e ${man}  ] ; then
 			docbook-to-man ${man} >${man%.sgml}.${i};
+			sed -i 's/^\(\.\\" created by instant \/ docbook-to-man\).*$/\1/' ${man%.sgml}.${i}
 		fi
 	done
 done
