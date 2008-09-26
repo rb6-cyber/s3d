eadb3ace349f2e514c2f87c63bc4b1400162e8ab
Sven Eckelmann <sven.eckelmann@gmx.de>
2008-09-26 19:25:15 +0000

    Change docbook compiler to xmlto
    We are using a xml based version of docbook and not the (original) sgml
    version. (Open)Jade will fail on most distributions when it tries to compile
    the xml docbook. The correct way to build them is to use xsl to transform the
    document into the target file.
    The usage of raw xsltproc depends too much on the environment and will
    definitely fail on some distributions. The easiest way to get the same
    functionality as before is to use xmlto - a bash script which will be offered
    by the distributions and should always know where the correct xsl files can
    be found.
    
    Signed-off-by: Sven Eckelmann <sven.eckelmann@gmx.de>

--- cmake/modules/FindDocbook.cmake
+++ cmake/modules/FindDocbook.cmake
@@ -2,83 +2,32 @@
 # This module finds if Docbook is installed and determines where the
 # executables are. This code sets the following variables:
 #
-#  DOCBOOK2DVI_COMPILER: path to the DVI convert
 #  DOCBOOK2HTML_COMPILER: path to the HTML convert
-#  DOCBOOK2MAN_COMPILER: path to the MAN convert
-#  DOCBOOK2PDF_COMPILER: path to the PDF convert
-#  DOCBOOK2PS_COMPILER: path to the PS convert
-#  DOCBOOK2RTF_COMPILER: path to the RTF convert
-#  DOCBOOK2TEX_COMPILER: path to the TEX convert
-#  DOCBOOK2TEXI_COMPILER: path to the TEXI convert
-#  DOCBOOK2TXT_COMPILER: path to the TXT convert
 #
 
-set(DOCBOOK_BINARY_PATH ${DOCBOOK_BINARY_PATH}
-	CACHE PATH "Path to the Docbook binary directory."
-)
-
-find_program(DOCBOOK2DVI_COMPILER
-	NAMES docbook2dvi
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
-
 find_program(DOCBOOK2HTML_COMPILER
-	NAMES docbook2html
+	NAMES xmlto
 	PATHS ${DOCBOOK_BINARY_PATH}
 		/usr/bin
 )
-
-find_program(DOCBOOK2MAN_COMPILER
-	NAMES docbook2man
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
+set(DOCBOOK2HTML_ARGS "xhtml")
 
 find_program(DOCBOOK2PDF_COMPILER
-	NAMES docbook2pdf
+	NAMES xmlto
 	PATHS ${DOCBOOK_BINARY_PATH}
 		/usr/bin
 )
+set(DOCBOOK2PDF_ARGS "pdf")
 
 find_program(DOCBOOK2PS_COMPILER
-	NAMES docbook2ps
+	NAMES xmlto
 	PATHS ${DOCBOOK_BINARY_PATH}
 		/usr/bin
 )
-
-find_program(DOCBOOK2RTF_COMPILER
-	NAMES docbook2rtf
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
-
-find_program(DOCBOOK2TEX_COMPILER
-	NAMES docbook2tex
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
-
-find_program(DOCBOOK2TEXI_COMPILER
-	NAMES docbook2texi
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
-
-find_program(DOCBOOK2TXT_COMPILER
-	NAMES docbook2txt
-	PATHS ${DOCBOOK_BINARY_PATH}
-		/usr/bin
-)
+set(DOCBOOK2PS_ARGS "ps")
 
 mark_as_advanced(
-	DOCBOOK2DVI_COMPILER
 	DOCBOOK2HTML_COMPILER
-	DOCBOOK2MAN_COMPILER
 	DOCBOOK2PDF_COMPILER
 	DOCBOOK2PS_COMPILER
-	DOCBOOK2RTF_COMPILER
-	DOCBOOK2TEX_COMPILER
-	DOCBOOK2TEXI_COMPILER
-	DOCBOOK2TXT_COMPILER
 )
