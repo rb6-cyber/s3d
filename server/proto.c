rb6-cyber/s3d

commit c70779e87f0b40192c5d14f254da32238ac978ea

server: Force null termination of string after strncpy
strncpy doesn't terminate the string with a '\0' character when the length
of the destination memory location was shorter than the source string.
Accessing it again with string related functions isn't safe after such a
semi-failed copy and the caller has to handle it. The easiest way is to
always set the last character in the destination buffer to '\0' after the
strncpy was called.

server/proto.c
@@ -68,6 +68,7 @@ int prot_com_in(struct t_process *p, uint8_t * pbuf)
 		else
 			i = length;
 		strncpy(name, (char *)buf, i);
+		name[i - 1] = '\0';
 		s3dprintf(LOW, "[%d]\"%s\" logged in", p->id, name);
 		if (NULL == (np = process_protinit(p, name)))
 			event_quit(p);	/*  couldn't get process */

server/event.c
@@ -170,14 +170,17 @@ int event_obj_info(struct t_process *p, int32_t oid)
 		switch (o->oflags & OF_TYPE) {
 		case OF_VIRTUAL:
 			ap = get_proc_by_pid(o->virtual_pid);
-			strncpy(mo.name, ap->name, S3D_NAME_MAX);
+			strncpy(mo.name, ap->name, sizeof(mo.name));
+			mo.name[sizeof(mo.name) - 1] = '\0';
 			break;
 		case OF_CAM:
 			mo.scale = (float)((float)winw) / winh;	/* give aspect ratio to program */
-			strncpy(mo.name, "sys_camera0", S3D_NAME_MAX);
+			strncpy(mo.name, "sys_camera0", sizeof(mo.name));
+			mo.name[sizeof(mo.name) - 1] = '\0';
 			break;
 		case OF_POINTER:
-			strncpy(mo.name, "sys_pointer0", S3D_NAME_MAX);
+			strncpy(mo.name, "sys_pointer0", sizeof(mo.name));
+			mo.name[sizeof(mo.name) - 1] = '\0';
 			break;
 
 		}

server/mcp.c
@@ -77,7 +77,8 @@ int mcp_rep_object(int32_t mcp_oid)
 
 	htonfb(&mo.trans_x, 4);
 	ap = get_proc_by_pid(p->object[mcp_oid]->virtual_pid);
-	strncpy(mo.name, ap->name, S3D_NAME_MAX);
+	strncpy(mo.name, ap->name, sizeof(mo.name));
+	mo.name[sizeof(mo.name) - 1] = '\0';
 	prot_com_out(p, S3D_P_MCP_OBJECT, (uint8_t *) & mo, sizeof(struct mcp_object));
 	return 0;
 }

server/process.c
@@ -64,7 +64,8 @@ struct t_process *process_protinit(struct t_process *p, const char *name)
 			return NULL;
 		}
 	} else {
-		strncpy(p->name, name, S3D_NAME_MAX);
+		strncpy(p->name, name, sizeof(p->name));
+		p->name[sizeof(p->name) - 1] = '\0';
 		process_sys_init(p);
 
 		/* register the new process in the mcp */
@@ -229,7 +230,8 @@ int process_init(void)
 	procs_p = NULL;
 	process_add();
 	/* set up mcp */
-	strncpy(mcp_p->name, "mcp", S3D_NAME_MAX);
+	strncpy(mcp_p->name, "mcp", sizeof(mcp_p->name));
+	mcp_p->name[sizeof(mcp_p->name) - 1] = '\0';
 	mcp_p->con_type = CON_NULL;
 	process_sys_init(mcp_p);
 	return 0;

commit 5f7b9b342d48e1c7eaf24fee8fcf6c81cd391640

server: Fix handling of S3D_P_C_INIT name buffer
Fixes: c70779e87f0b ("server: Force null termination of string after strncpy")

server/proto.c
@@ -68,7 +68,7 @@ int prot_com_in(struct t_process *p, uint8_t * pbuf)
 		else
 			i = length;
 		strncpy(name, (char *)buf, i);
-		name[i - 1] = '\0';
+		name[sizeof(name) - 1] = '\0';
 		s3dprintf(LOW, "[%d]\"%s\" logged in", p->id, name);
 		if (NULL == (np = process_protinit(p, name)))
 			event_quit(p);	/*  couldn't get process */